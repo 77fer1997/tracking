a744be9f044e201a033fefb4e9bbf58070d4194d
LuisCT <77fer1997@gmail.com>
2021-08-23 14:06:34 -0500

    Avance

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Video from "../../videos/video3.mp4";
+import { Button } from "../ButtonElement";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroBtnWrapper,
+  HeroH1,
+  HeroP,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer id="home">
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and receive $250 in cred towards your
+          next payment.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary
+            dark
+          >
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
